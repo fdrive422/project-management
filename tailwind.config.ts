15eee28d3eb4dce35ac8f535d928ede43059a8ee
fdrive422 <fafrago@gmail.com>
2025-03-05 20:28:44 -0800

    update tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,8 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  darkMode: "class",
   content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
@@ -38,3 +39,4 @@ module.exports = {
   },
   plugins: [],
 };
+export default config;
