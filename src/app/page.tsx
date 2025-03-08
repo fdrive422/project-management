00de944f1ef490cb34051ca45793b70dab01af4e
fdrive422 <fafrago@gmail.com>
2025-03-08 13:37:49 -0800

    build dashboardWrapper, update laybout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <div className="text-4xl font-bold text-blue-600 hover:text-blue-800 transition-colors">
+      <div className="text-4xl font-bold text-blue-600 transition-colors hover:text-blue-800">
         Hello World
       </div>
       Welcome to my Next.js app with Tailwind CSS
