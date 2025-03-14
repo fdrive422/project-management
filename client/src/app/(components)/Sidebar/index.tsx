57dc24aaf87bb3ebe417e4da0e198fca7ca183b3
fdrive422 <fafrago@gmail.com>
2025-03-13 18:37:06 -0700

    first commit

---
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import React, { useState } from "react";
+
+const Sidebar = () => {
+  const [showProjects, setShowProjects] = useState(true);
+  const [showPriority, setShowPriority] = useState(true);
+
+  const sidebarClassNames = `fixed flex flex-col h-[100%] justify-between shadow-xl
+    transition-all duration-300 h-full z-40 dark:bg-black overflow-y-auto bg-white`;
+
+  return (
+    <div className={sidebarClassNames}>
+      <div className="flex h-[100%] w-full flex-col justify-start">
+        {/* TOP LOGO */}
+        <div className="z-50 flex min-h-[56px] w-64 items-center justify-between bg-white px-6 pt-3 dark:bg-black">
+          <div className="text-xl font-bold text-gray-800 dark:text-white">
+            MAIN-LIST
+          </div>
+        </div>
+        {/* TEAMS */}
+        <div className="flex items-center gap-5 border-gray-100 border-y-[1.5] px-8 py-4 dark:border-gray-700">
+          <Image src="/logo1.png" alt="Logo" width={80} height={80} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
---
+++ client/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <div className="text-2xl font-bold text-blue-600 transition-colors hover:text-blue-800">
+        Welcome to my Next.js app with Tailwind CSS
+      </div>
+    </main>
+  );
+}
