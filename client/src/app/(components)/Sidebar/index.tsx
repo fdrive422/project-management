38df5b2b5e2260fef25f00fe58b068c6c87de54a
fdrive422 <fafrago@gmail.com>
2025-04-06 10:58:58 -0700

    sidebar collapsed

--- client/src/app/(components)/Sidebar/index.tsx
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -4,13 +4,21 @@ import React, { useState } from "react";
 import { usePathname } from "next/navigation";
 import { useAppDispatch, useAppSelector } from "@/app/redux";
 import Link from "next/link";
+import { setIsSidebarCollapsed } from "@/state";
 
 const Sidebar = () => {
   const [showProjects, setShowProjects] = useState(true);
   const [showPriority, setShowPriority] = useState(true);
 
+  const dispatch = useAppDispatch();
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed,
+  );
+
   const sidebarClassNames = `fixed flex flex-col h-[100%] justify-between shadow-xl
-    transition-all duration-300 h-full z-40 dark:bg-black overflow-y-auto bg-white`;
+    transition-all duration-300 h-full z-40 dark:bg-black overflow-y-auto bg-white
+    ${isSidebarCollapsed ? "w-0 hidden" : "w-64"}
+    `;
 
   return (
     <div className={sidebarClassNames}>
@@ -20,13 +28,24 @@ const Sidebar = () => {
           <div className="text-xl font-bold text-gray-800 dark:text-white">
             MAIN-LIST
           </div>
+          {isSidebarCollapsed ? null : (
+            <button
+              className="py-3"
+              onClick={() => {
+                dispatch(setIsSidebarCollapsed(!isSidebarCollapsed));
+              }}
+            ></button>
+          )}
         </div>
         {/* TEAMS */}
         <div className="flex items-center gap-5 border-y-[1.5px] border-gray-100 px-8 py-4 dark:border-gray-700">
           <Image src="/logo.png" alt="Logo" width={40} height={40} />
           <div>
             <h3 className="text-md font-bold tracking-widest dark:text-gray-200">
-              EXCEL TEAM
+              <div className="fond-bold text-xl text-gray-800 dark:text-white">
+                EXCEL TEAM
+              </div>
+
               <div className="mt-1 flex items-start gap-2">
                 <LockIcon className="mt-[0.1rem] h-3 w-3 text-gray-500 dark:text-gray-400" />
                 <p className="text-xs text-gray-500">Private</p>
