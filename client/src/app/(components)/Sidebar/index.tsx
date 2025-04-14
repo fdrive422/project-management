9ed5caedbd2f6d0e85fcc07576929b66e8c4e3f6
fdrive422 <fafrago@gmail.com>
2025-04-13 20:32:00 -0700

    add sidebar links and icons

--- client/src/app/(components)/Sidebar/index.tsx
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -1,4 +1,17 @@
-import { Home, Icon, LockIcon, LucideIcon } from "lucide-react";
+import {
+  Briefcase,
+  ChevronDown,
+  ChevronUp,
+  Home,
+  Icon,
+  LockIcon,
+  LucideIcon,
+  Search,
+  Settings,
+  User,
+  Users,
+  X,
+} from "lucide-react";
 import Image from "next/image";
 import React, { useState } from "react";
 import { usePathname } from "next/navigation";
@@ -26,7 +39,7 @@ const Sidebar = () => {
         {/* TOP LOGO */}
         <div className="z-50 flex min-h-[56px] w-64 items-center justify-between bg-white px-6 pt-3 dark:bg-black">
           <div className="text-xl font-bold text-gray-800 dark:text-white">
-            MAIN LIST
+            MAIN-LIST
           </div>
           {isSidebarCollapsed ? null : (
             <button
@@ -34,18 +47,19 @@ const Sidebar = () => {
               onClick={() => {
                 dispatch(setIsSidebarCollapsed(!isSidebarCollapsed));
               }}
-            ></button>
+            >
+              <X className="h-6 w-6 text-gray-800 hover:text-gray-500 dark:text-white" />
+            </button>
           )}
         </div>
         {/* TEAMS */}
         <div className="flex items-center gap-5 border-y-[1.5px] border-gray-100 px-8 py-4 dark:border-gray-700">
           <Image src="/logo.png" alt="Logo" width={40} height={40} />
           <div>
-            <h3 className="text-md font-bold tracking-widest dark:text-gray-200">
-              <div className="fond-bold text-xl text-gray-800 dark:text-white">
-                EXCEL TEAM
-              </div>
-
+            <h3 className="text-md font-bold tracking-wide dark:text-gray-200">
+              {/* <div className="fond-bold text-xl text-gray-800 dark:text-white"> */}
+              EXCEL TEAM
+              {/* </div> */}
               <div className="mt-1 flex items-start gap-2">
                 <LockIcon className="mt-[0.1rem] h-3 w-3 text-gray-500 dark:text-gray-400" />
                 <p className="text-xs text-gray-500">Private</p>
@@ -56,7 +70,24 @@ const Sidebar = () => {
         {/* NAVBAR LINKS */}
         <nav className="z-10 w-full">
           <SidebarLink icon={Home} label="Home" href="/" />
+          <SidebarLink icon={Briefcase} label="Timeline" href="/timeline" />
+          <SidebarLink icon={Search} label="Search" href="/search" />
+          <SidebarLink icon={Settings} label="Settings" href="/settings" />
+          <SidebarLink icon={User} label="Users" href="/users" />
+          <SidebarLink icon={Users} label="Teams" href="/teams" />
         </nav>
+        <button
+          onClick={() => setShowProjects((prev) => !prev)}
+          className="pu-3 flex w-full items-center justify-between px-8 text-gray-500"
+        >
+          <span className="">Projects</span>
+          {showProjects ? (
+            <ChevronUp className="h-5 w-5" />
+          ) : (
+            <ChevronDown className="h-5 w-5" />
+          )}
+        </button>
+        {/* PROJECTS LIST */}
       </div>
     </div>
   );
@@ -66,7 +97,6 @@ interface SidebarLinkProps {
   href: string;
   icon: LucideIcon;
   label: string;
-  // isCollapsed: boolean;
 }
 
 const SidebarLink = ({
@@ -78,17 +108,13 @@ const SidebarLink = ({
   const pathname = usePathname();
   const isActive =
     pathname === href || (pathname === "/" && href === "/dashboard");
-  const screenWidth = window.innerWidth;
-
-  const dispatch = useAppDispatch();
-  const isSidebarCollapsed = useAppSelector(
-    (state) => state.global.isSidebarCollapsed,
-  );
 
   return (
     <Link href={href} className="w=full">
       <div
-        className={`relative flex cursor-pointer items-center gap-3 transition-colors hover:bg-gray-100 dark:bg-black dark:hover:bg-gray-700 ${isActive ? "bg-gray-100 text-white dark:bg-gray-600" : ""}`}
+        className={`relative flex cursor-pointer items-center gap-3 transition-colors hover:bg-gray-100 dark:bg-black dark:hover:bg-gray-700 ${
+          isActive ? "bg-gray-100 text-white dark:bg-gray-600" : ""
+        }justify-start px-8 py-3`}
       >
         {isActive && (
           <div className="w-[5px absolute left-0 top-0 h-[100%] bg-blue-200" />
